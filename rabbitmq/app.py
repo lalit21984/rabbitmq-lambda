30a8dbc95a00a179fc3e574e9d1b9701e8354f93
Lalit Sahu <lksahu@amazon.com>
2023-07-10 10:39:03 -0500

    initial check-in

---
+++ rabbitmq/app.py
@@ -0,0 +1,44 @@
+import cfnresponse
+import boto3
+import json
+import os
+import requests
+import logging
+
+secret_client = boto3.client('secretsmanager')
+logging.basicConfig(level=logging.INFO)
+
+def handler(event, context):
+  # Print event and context to allow manual success or failure reporting
+  print(event)
+  print(context.log_stream_name)
+  
+  if event["RequestType"] in ["Create", "Update"]:
+    try:
+      # Grab config file and env variables
+
+      config_path = os.environ['LAMBDA_TASK_ROOT'] + "/rabbit_config_" + os.environ['ENVIRONMENT'] + ".json"
+      json_config = json.loads(open(config_path).read())
+      secret_arn = os.environ['SECRET_ARN']
+      rabbit_endpoint = os.environ['RABBIT_ENDPOINT']
+      
+
+      # Get secret from secrets manager
+      response = secret_client.get_secret_value(SecretId=secret_arn)
+
+      # Send config file to RabbitMQ api
+      requests.post(url="https://" + rabbit_endpoint + "/api/definitions", json=json_config, auth=('rabbit-admin', response['SecretString']))
+
+      # Report success
+      cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+
+    except Exception as e:
+      # Log error and report failure
+      logging.exception("Failed to configure RabbitMQ")
+      cfnresponse.send(event, context, cfnresponse.FAILED, {})
+
+  elif event["RequestType"] == "Delete":
+    # Do nothing on delete except report success
+    cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+
+
